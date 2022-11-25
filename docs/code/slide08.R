68a8ac758c27ebc7e7182da275143da84d55d66f
Noboru Murata <noboru.murata@gmail.com>
2022-11-25 14:22:55 +0900

    2022 version

--- docs/code/slide08.R
+++ docs/code/slide08.R
@@ -9,15 +9,15 @@
 ### 東京の気象データによる判別分析
 library(MASS)
 ## データの整理
-TW.data <- read.csv("data/tokyo_weather.csv")
-TW.subset  <- subset(TW.data,
+tw_data <- read.csv("data/tokyo_weather.csv")
+tw_subset  <- subset(tw_data,
                      subset= month %in% c(10,11),
                      select=c(temp,humid,month))
 idx <- seq(2,60,by = 2)
-TW.train <- TW.subset[ idx,] # 訓練データ
-TW.test  <- TW.subset[-idx,] # 試験データ
+tw_train <- tw_subset[ idx,] # 訓練データ
+tw_test  <- tw_subset[-idx,] # 試験データ
 ## 視覚化
-with(TW.subset, 
+with(tw_subset, 
      plot(temp, humid, # 散布図の作成
           pch=month, col=month,
           xlab="temperature",ylab="humidity",
@@ -25,30 +25,30 @@ with(TW.subset,
 legend("bottomright",inset=.05, # 凡例の作成
        pch=c(10,11), col=c(10,11), legend=c("Oct","Nov"))
 ## 訓練データで判別関数を作成．等分散性を仮定
-TW.lda <- lda(month ~ temp + humid, data=TW.train)
-plot(TW.lda) # 訓練データの判別関数値
-TW.est <- predict(TW.lda) # 判別関数によるクラス分類結果の取得
-table(true=TW.train$month, pred=TW.est$class) # 真値と予測値の比較
+tw_lda <- lda(month ~ temp + humid, data=tw_train)
+plot(tw_lda) # 訓練データの判別関数値
+tw_est <- predict(tw_lda) # 判別関数によるクラス分類結果の取得
+table(true=tw_train$month, pred=tw_est$class) # 真値と予測値の比較
 ## 試験データによる評価
-TW.pred <- predict(TW.lda, newdata=TW.test) 
-table(true=TW.test$month, pred=TW.pred$class) # 真値と予測値の比較
-TW.pred$class 
-TW.test$month 
+tw_pred <- predict(tw_lda, newdata=tw_test) 
+table(true=tw_test$month, pred=tw_pred$class) # 真値と予測値の比較
+tw_pred$class 
+tw_test$month 
 ## 判別結果の図示
 myLine <- function(z) { # 判別境界を引くための関数
     a0<-as.vector(colMeans(z$means) %*% z$scaling)
     a<-c(a0/z$scaling[2],-z$scaling[1]/z$scaling[2])
     return(a)
 }
-with(TW.test, 
+with(tw_test, 
      plot(temp, humid, # 試験データの散布図
           pch=month, col=month,
           xlab="temperature",ylab="humidity",
           main="Oct. & Nov"))
-with(TW.train, 
+with(tw_train, 
      points(temp, humid, # 訓練データの散布図
             pch=month, col=month+3))
-abline(myLine(TW.lda), col="blue", lwd=2)
+abline(myLine(tw_lda), col="blue", lwd=2)
 
 ### 
 ### 練習問題 2次判別
@@ -57,27 +57,27 @@ abline(myLine(TW.lda), col="blue", lwd=2)
 ### 東京の気象データによる判別分析
 library(MASS)
 ## データの整理 (前に実行している場合は不要)
-TW.data <- read.csv("data/tokyo_weather.csv")
-TW.subset  <- subset(TW.data,
+tw_data <- read.csv("data/tokyo_weather.csv")
+tw_subset  <- subset(tw_data,
                      subset= month %in% c(10,11),
                      select=c(temp,humid,month))
 idx <- seq(2,60,by = 2)
-TW.train <- TW.subset[ idx,] # 訓練データ
-TW.test  <- TW.subset[-idx,] # 試験データ
+tw_train <- tw_subset[ idx,] # 訓練データ
+tw_test  <- tw_subset[-idx,] # 試験データ
 ## 訓練データで判別関数を作成
-TW.qda <- qda(month ~ temp + humid, data=TW.train)
-TW.est2 <- predict(TW.qda) # 判別関数によるクラス分類結果の取得
-table(true=TW.train$month, pred=TW.est2$class) # 真値と予測値の比較
+tw_qda <- qda(month ~ temp + humid, data=tw_train)
+tw_est2 <- predict(tw_qda) # 判別関数によるクラス分類結果の取得
+table(true=tw_train$month, pred=tw_est2$class) # 真値と予測値の比較
 ## 試験データによる評価
-TW.pred2 <- predict(TW.qda, newdata=TW.test) 
-table(true=TW.test$month, pred=TW.pred2$class) # 真値と予測値の比較
-TW.pred2$class 
-TW.test$month 
+tw_pred2 <- predict(tw_qda, newdata=tw_test) 
+table(true=tw_test$month, pred=tw_pred2$class) # 真値と予測値の比較
+tw_pred2$class 
+tw_test$month 
 ## 判別結果の図示
 ## 判別境界を描くのは複雑なので，色と形で代用する
-with(TW.test, 
+with(tw_test, 
      plot(temp, humid, # 試験データの散布図
-          pch=as.numeric(TW.pred2$class),
+          pch=as.numeric(tw_pred2$class),
           col=month,
           xlab="temperature",ylab="humidity",
           main="Oct. & Nov"))
@@ -89,32 +89,32 @@ with(TW.test,
 ### 東京の気象データによる判別分析
 library(MASS)
 ## データの整理 (前に実行している場合は不要)
-TW.data <- read.csv("data/tokyo_weather.csv")
-TW.subset  <- subset(TW.data,
+tw_data <- read.csv("data/tokyo_weather.csv")
+tw_subset  <- subset(tw_data,
                      subset= month %in% c(9,10,11),
                      select=c(temp,humid,month))
 ## 判別関数を作成
-TW.lda3 <- lda(month ~ temp + humid, data=TW.subset)
-TW.est3 <- predict(TW.lda3) # 判別関数によるクラス分類結果の取得
-table(true=TW.subset$month, pred=TW.est3$class) # 真値と予測値の比較
-plot(TW.lda3, col=TW.subset$month) # 判別関数値の図示
+tw_lda3 <- lda(month ~ temp + humid, data=tw_subset)
+tw_est3 <- predict(tw_lda3) # 判別関数によるクラス分類結果の取得
+table(true=tw_subset$month, pred=tw_est3$class) # 真値と予測値の比較
+plot(tw_lda3, col=tw_subset$month) # 判別関数値の図示
 
 ## 12ヶ月分のデータを用いる
 ## 数が多いのでサンプリングする
-idx <- sample(nrow(TW.data), 100)
-TW.multi <- lda(month ~ temp + solar + wind + humid,
-                data=TW.data[idx,])
-plot(TW.multi, col=TW.data[idx,]$month)
+idx <- sample(nrow(tw_data), 100)
+tw_multi <- lda(month ~ temp + solar + wind + humid,
+                data=tw_data[idx,])
+plot(tw_multi, col=tw_data[idx,]$month)
 ## 特徴量は説明変数の数までしか作成できないので，精度は低いことがわかる
 
 ## 雨の有無を識別する例
-TW.rdata <- transform(TW.data,
+tw_rdata <- transform(tw_data,
                       rain=factor(rain>0), # 雨の有無でラベル化する
                       month=factor(month)) # 月ごとの気候の違いの補正のため
-TW.rain <- lda(rain ~ temp + solar + wind + month,
-               data=TW.rdata,
+tw_rain <- lda(rain ~ temp + solar + wind + month,
+               data=tw_rdata,
                subset=idx) # 一部のデータで推定，12ヶ月分の例とは別の指定の仕方
-plot(TW.rain)
-TW.rpred <- predict(TW.rain, newdata=TW.rdata) # 全データを予測
-table(true=TW.rdata$rain[idx], est=TW.rpred$class[idx])
-table(true=TW.rdata$rain[-idx], est=TW.rpred$class[-idx])
+plot(tw_rain)
+tw_rpred <- predict(tw_rain, newdata=tw_rdata) # 全データを予測
+table(true=tw_rdata$rain[idx], est=tw_rpred$class[idx])
+table(true=tw_rdata$rain[-idx], est=tw_rpred$class[-idx])
