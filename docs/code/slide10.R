16fca1663992ec3829c2a9a267943e95772b78a0
Noboru Murata <noboru.murata@gmail.com>
2022-11-29 14:53:21 +0900

    2022 version

--- docs/code/slide10.R
+++ docs/code/slide10.R
@@ -10,22 +10,22 @@
 library(cluster) # require(cluster)
 
 ## データの読み込み
-JS.data <- read.csv(file="data/japan_social.csv", row.names=1)
+js_data <- read.csv(file="data/japan_social.csv", row.names=1)
 
 ## ユークリッド距離とマンハッタン距離の計算
-dst.euc <- dist(JS.data, method="euclidean")
-dst.man <- dist(JS.data, method="manhattan")
-dsy.euc <- daisy(JS.data, metric="euclidean")
-dsy.man <- daisy(JS.data, metric="manhattan")
+dst.euc <- dist(js_data, method="euclidean")
+dst.man <- dist(js_data, method="manhattan")
+dsy.euc <- daisy(js_data, metric="euclidean")
+dsy.man <- daisy(js_data, metric="manhattan")
 ## 両者が同じことを確認
 as.matrix(dst.euc)[1:5,1:5]
 as.matrix(dsy.euc)[1:5,1:5]
 
 ## 正規化したユークリッド距離とマンハッタン距離の計算
-dst.euc <- dist(scale(JS.data), method="euclidean")
-dst.man <- dist(scale(JS.data), method="manhattan")
-dsy.euc <- daisy(JS.data, metric="euclidean", stand=TRUE)
-dsy.man <- daisy(JS.data, metric="manhattan", stand=TRUE)
+dst.euc <- dist(scale(js_data), method="euclidean")
+dst.man <- dist(scale(js_data), method="manhattan")
+dsy.euc <- daisy(js_data, metric="euclidean", stand=TRUE)
+dsy.man <- daisy(js_data, metric="manhattan", stand=TRUE)
 ## 正規化の方法が異なることに注意
 as.matrix(dst.man)[10:15,10:15]
 as.matrix(dsy.man)[10:15,10:15]
@@ -33,7 +33,7 @@ as.matrix(dsy.man)[10:15,10:15]
 ## 以下 daisy による正規化を用いる
 ## 関東の都県同士の距離を表示しなさい
 str(dsy.euc) # 距離行列のもつ情報を見る
-attr(dsy.euc, "Labels") # 県名を確認 (rownames(JS.data)でも良い)
+attr(dsy.euc, "Labels") # 県名を確認 (rownames(js_data)でも良い)
 as.matrix(dsy.euc)[8:14, 8:14]
 as.matrix(dsy.man)[8:14, 8:14]
 ## dist/dissimilarity オブジェクトは距離以外の様々な属性 (attributes) を持つ
@@ -59,47 +59,47 @@ plot(dsy.euc, dsy.man,
 ### 
 
 ## クラスタリングの実行
-JS.dst <- dist(scale(JS.data)) # 正規化してユークリッド距離を測る
-JS.est <- hclust(JS.dst, method="average") # 群平均法
-plot(JS.est,
+js_dst <- dist(scale(js_data)) # 正規化してユークリッド距離を測る
+js_est <- hclust(js_dst, method="average") # 群平均法
+plot(js_est,
      cex=0.8, # 文字の大きさを調整
      sub="", xlab="", # 表示の一部を消去
      main="euclidean + average") # デンドログラムの表示
 
 ## クラスタの分割
 k <- 5 # 分割数を指定
-plot(JS.est,
+plot(js_est,
      hang=-1, # ラベルを揃えて表示
      cex=0.8, 
      sub="", xlab="", main="")
-rect.hclust(JS.est, k=k, border="orange") 
+rect.hclust(js_est, k=k, border="orange") 
 ## 結果の確認 (各クラスタ内の県名を表示)
-JS.clst <- cutree(JS.est, k=k) # デンドログラムを分割
-JS.pref <- rownames(JS.data) # 県名の取得
+js_clst <- cutree(js_est, k=k) # デンドログラムを分割
+js_pref <- rownames(js_data) # 県名の取得
 for(i in 1:k){
     cat("=== cluster",i,"===\n")
-    print(JS.pref[JS.clst==i])
+    print(js_pref[js_clst==i])
 }
 
 ## 主成分分析を併用して表示 (参考)
-JS.pca <- prcomp(JS.data, scale.=TRUE) # データを正規化      
-plot(predict(JS.pca),
-     pch=JS.clst, # クラスタ毎に形を変える
+js_pca <- prcomp(js_data, scale.=TRUE) # データを正規化      
+plot(predict(js_pca),
+     pch=js_clst, # クラスタ毎に形を変える
      col="gray") 
-text(predict(JS.pca), 
-     labels=paste0("  ",rownames(JS.data)), # 空白を加えて県名を表示
+text(predict(js_pca), 
+     labels=paste0("  ",rownames(js_data)), # 空白を加えて県名を表示
      adj=c(0,0.5), # ラベルの位置をxは右寄せ(0)，yは真中(0.5)に指定
-     col=JS.clst, # クラスタ毎に色を変える
+     col=js_clst, # クラスタ毎に色を変える
      cex=0.8) # 文字の大きさを調整
 ## 最大クラスタを再評価
-table(JS.clst) # 最大を確認
-m <- which.max(table(JS.clst)) # 最大クラスタの番号を取り出す
-JS.pca <- prcomp(JS.data[JS.clst==m,], scale.=TRUE) # 最大クラスタのみ処理
-plot(predict(JS.pca),
+table(js_clst) # 最大を確認
+m <- which.max(table(js_clst)) # 最大クラスタの番号を取り出す
+js_pca <- prcomp(js_data[js_clst==m,], scale.=TRUE) # 最大クラスタのみ処理
+plot(predict(js_pca),
      pch=m, # クラスタの形を指定
      col="gray") 
-text(predict(JS.pca),
-     labels=paste0("  ",rownames(JS.data[JS.clst==m,])),
+text(predict(js_pca),
+     labels=paste0("  ",rownames(js_data[js_clst==m,])),
      adj=c(0,0.5), 
      col=m, # クラスタの色を指定
      cex=0.8)
@@ -112,18 +112,18 @@ text(predict(JS.pca),
 library(cluster) # 既に読み込んでいれば不要
 
 ## データの読み込み("omusubi.csv"を用いる)
-OM.data <- read.csv(file="data/omusubi.csv", row.names=1)
+om_data <- read.csv(file="data/omusubi.csv", row.names=1)
 
 ## データの視覚化
 ## pairs plot
-pairs(OM.data,
+pairs(om_data,
       col="blue",
       panel=panel.smooth, # 各散布図の傾向を見る回帰曲線を付加
       main="Favorite Filling in Omusubi (2009)")
 ## bar plot
-barplot(t(as.matrix(OM.data)), # barplot用にデータフレームを変換
+barplot(t(as.matrix(om_data)), # barplot用にデータフレームを変換
         col=rainbow(8), # 具材ごとに色を変える
-        legend.text=colnames(OM.data), # 色の凡例を付加
+        legend.text=colnames(om_data), # 色の凡例を付加
         args.legend=list(cex=0.6), # 凡例の大きさを調整
         horiz=TRUE, # 横向きで作成
         las=1, # ラベルを水平に表示
@@ -133,18 +133,18 @@ barplot(t(as.matrix(OM.data)), # barplot用にデータフレームを変換
         main="Favorite Filling in Omusubi (2009)")
 
 ## Hellinger距離の計算
-OM.dsy <- 1/sqrt(2)*daisy(sqrt(OM.data/100))
-## 定数倍を気にしないのであれば daisy(sqrt(OM.data)) でよい
+om_dsy <- 1/sqrt(2)*daisy(sqrt(om_data/100))
+## 定数倍を気にしないのであれば daisy(sqrt(om_data)) でよい
 
 ## 階層的クラスタリング
-OM.agns <- agnes(OM.dsy) 
-plot(OM.agns, which.plot=2, cex=0.8,
+om_agns <- agnes(om_dsy) 
+plot(om_agns, which.plot=2, cex=0.8,
      main="Dendrogram of Omusubi Data")
 
 ## クラスタ数7として2次元のクラスタ表示
 k <- 7
-clusplot(x=OM.data,
-         clus=cutree(OM.agns, k=k),
+clusplot(x=om_data,
+         clus=cutree(om_agns, k=k),
          labels=2,
          col.p="green", col.txt="blue", col.clus="orange", cex=0.8,
          main="Cluster of Omusubi Data")
